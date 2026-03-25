dde837976a7a63e12120b216362feda3a18ed133
Pushkar Banjare <157865240+pushkarbanjare@users.noreply.github.com>
2026-03-25 17:13:34 +0530

    cors updated

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://hiremind-tool.vercel.app"],
+    allow_origins=["http://localhost:3000", "https://hiremind-frontend-voin.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
