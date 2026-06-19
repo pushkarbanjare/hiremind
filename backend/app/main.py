f1df6b5d72b70c474fc250d1181c57c9be390049
Pushkar Banjare <banjarepushkar.2k3@gmail.com>
2026-06-20 00:07:17 +0530

    prod fix#1

--- backend/app/main.py
+++ backend/app/main.py
@@ -22,7 +22,7 @@ app.add_middleware(SlowAPIMiddleware)
 # ========== cors middleware ==========
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://hiremind-web.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
